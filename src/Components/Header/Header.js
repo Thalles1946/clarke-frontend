9e803674ff511f8e6afd4600ee8da4cf38914064
Thalles1946 <thallesaug2099@gmail.com>
2024-01-18 16:07:36 -0300

    Feat : Criação do Header Feat : Logo adicionada Feat : Alteração do ícone

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+import Logo from "./../../img/clarke-logo-verde.png";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <img src={Logo} alt="teste" />
+    </header>
+  );
+};
+
+export default Header;
